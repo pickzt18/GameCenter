ae41892073cbb164bf1458ceaba06a6401939ac0
Zachary Pick <pickzt18@gmail.com>
2022-03-04 09:20:32 -0800

    FINAL!

--- src/com/GuruGames/games/Wordle/Wordle.java
+++ src/com/GuruGames/games/Wordle/Wordle.java
@@ -1,4 +1,3 @@
-
 package com.GuruGames.games.Wordle;
 
 import com.GuruGames.games.Game;
@@ -22,6 +21,7 @@ public class Wordle implements Game {
     private final char[] wordle = WORD.toLowerCase().toCharArray();
     private final ArrayList<String> letters = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
     boolean[] checksPast = {false, false, false, false, false};
+    WordleGameData gameData = new WordleGameData();
     private char[] showCorrect = {'-', '-', '-', '-', '-'};
     private boolean gameOver = false;
     private boolean winner;
@@ -31,8 +31,7 @@ public class Wordle implements Game {
     private int cheatCount = 0;
     private char[] charGuess;
     private Boolean checkResults;
-    private boolean started=false;
-    WordleGameData gameData=new WordleGameData();
+    private boolean started = false;
 
     public Wordle() throws FileNotFoundException {
         System.out.println("Wordle: Enter start to begin, help to see rules, or quit to return to the main menu.");
@@ -76,8 +75,8 @@ public class Wordle implements Game {
 
                         System.out.println();
                     } else {
-                            checkGuess();
-                            printGuess();
+                        checkGuess();
+                        printGuess();
 
 
                         System.out.println();
@@ -99,58 +98,50 @@ public class Wordle implements Game {
     /**
      * Checks to see whether your guess is correct and adds "+","=","-" to an array to determine which color a letter should be printed in.
      */
-    protected void checkGuess(){
+    protected void checkGuess() {
+        showCorrect = new char[]{'-', '-', '-', '-', '-'};
+        checksPast = new boolean[]{false, false, false, false, false};
         for (int i = 0; i < wordle.length; i++) {
-            for (int j = 0; j < charGuess.length; j++) {
-                if (wordle[j] == (charGuess[i])) {
-                    if (i != j) {
-                        for (int k = 0; k <= i; k++) {
-                            if (checksPast[k]) {
-                                if (charGuess[i] == charGuess[k]) {
-                                    if (showCorrect[k] != '=') {
-                                        showCorrect[k] = '-';
-                                        charGood = true;
-                                    }
-                                }
-                            }
-                        }
-                        if (charGood) {
-                            showCorrect[i] = '+';
-                            checksPast[i] = true;
-                            break;
-                        }
-                        break;
-
-                    } else {
-                        if (i > 0) {
+            if (wordle[i] == charGuess[i]) {
+                showCorrect[i] = '=';
+                checksPast[i] = true;
+            }
+        }
+        for (int i = 0; i < wordle.length; i++) {
+            if (showCorrect[i] != '=') {
+                for (int j = 0; j < charGuess.length; j++) {
+                    if (wordle[j] == (charGuess[i])&&showCorrect[j]!='=') {
+                        if (i != j) {
                             for (int k = 0; k <= i; k++) {
                                 if (checksPast[k]) {
                                     if (charGuess[i] == charGuess[k]) {
-                                        if (showCorrect[k] != '=') {
+                                        if (showCorrect[k] != '=' && i != k ) {
                                             showCorrect[k] = '-';
                                             charGood = true;
                                         }
                                     }
+                                } else if (charGuess[i] == charGuess[k]) {
+                                    charGood = true;
+                                } else {
+                                    charGood = false;
                                 }
                             }
                             if (charGood) {
-                                showCorrect[i] = '=';
+                                showCorrect[i] = '+';
                                 checksPast[i] = true;
-                                break;
-                            }
-                            break;
+                                charGood = false;
 
-                        } else {
-                            showCorrect[i] = '=';
-                            checksPast[i] = true;
+                            }
                         }
+
                     }
                 }
-            }
 
+            }
         }
     }
-    protected void printGuess(){
+
+    protected void printGuess() {
         for (var c : charGuess) {
             letters.remove(String.valueOf(c));
         }
@@ -167,6 +158,7 @@ public class Wordle implements Game {
         System.out.println();
 
     }
+
     /**
      * @param wordGuess-Accepts an inputted guess parameter.
      * @return true-if the word exists in guesses.txt
@@ -206,7 +198,7 @@ public class Wordle implements Game {
             if (cheatCount == 1) {
                 System.out.println("But you cheated 1 time!");
             } else if (cheatCount > 1) {
-                gameData.cheatCount+=cheatCount;
+                gameData.cheatCount += cheatCount;
                 System.out.println("But you cheated " + cheatCount + " times!");
             }
             return true;
@@ -228,13 +220,13 @@ public class Wordle implements Game {
     public void parseCommand(String command, String... params) throws IllegalArgumentException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
         if (command.equalsIgnoreCase("help")) {
             System.out.println(help());
-        } else if (command.split("").length==5&&started) {
+        } else if (command.split("").length == 5 && started) {
             runGame(command);
-        } else if(command.equalsIgnoreCase("start")&&!started) {
-            started=true;
+        } else if (command.equalsIgnoreCase("start") && !started) {
+            started = true;
             System.out.println("Enter a 5 letter guess. If the wordle contains the letter it will be printed in yellow. If the letter is in the correct spot it will be printed in green.");
             throw new IllegalArgumentException("");
-        }else{
+        } else {
             throw new IllegalArgumentException("Please enter a 5 letter guess.");
         }
     }
